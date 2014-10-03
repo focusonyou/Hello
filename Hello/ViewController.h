207b8681f257cd06a7ed687ae0720e9a8b5ad5c6
jay <jayden.lin@udngroup.com>
2014-10-03 18:08:12 +0800

    new project

---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by UDN08600-D001 on 2014/10/3.
+//  Copyright (c) 2014年 udn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hello/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by UDN08600-D001 on 2014/10/3.
+//  Copyright (c) 2014年 udn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
